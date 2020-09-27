d2e1af29d0c1ef65d4dc08af1a8b11caa4175758
Meghanand Dussa <ds.meghaanand@gmial.com>
2020-09-28 00:07:36 +0530

    Easy file move powershell script

---
+++ File_Move/Easy_file_move_AS-per-Type.ps1
@@ -0,0 +1,22 @@
+﻿$_path=$null;
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+[string]$_path = [Microsoft.VisualBasic.Interaction]::InputBox("Please enter the path of the files to be sorted", "Read-Host")
+
+if($_path -ne $null){
+
+
+$file_types=Get-ChildItem $_path -Recurse |Where-Object{($_.PSIsContainer -match "False") -and ($_.Extension -ne "")} | Select-Object -Property Extension -Unique
+
+foreach($fltype in $file_types.Extension){
+$fldrnme= $fltype.ToString().split(".")[1];
+if((Test-Path "$_path\FIles_As_per_Types\$fldrnme") -eq $false){
+New-Item -Path "$_path\FIles_As_per_Types\" -Name $fltype.ToString().split(".")[1] -ItemType Directory -Force -Verbose
+}
+
+Get-ChildItem $_path\ -Recurse |Where-Object{($_.DirectoryName -notlike "*$fldrnme*") -and ($_.Extension -match $fltype)} |Move-Item -Destination "$_path\FIles_As_per_Types\$fldrnme" -Force -Verbose
+
+
+}
+}
+
+
---
+++ File_Move/Moving_AAE_file.ps1
@@ -0,0 +1,21 @@
+﻿<#$fldr=Get-ChildItem 'E:\Dubai Vacation\cloud\' | select Name
+
+$AAE_file= Get-ChildItem 'E:\Dubai Vacation\Apple_All_PhotosVideos\AAE' | Select BaseName
+
+foreach($AAE in $AAE_file.BaseName){
+
+foreach($othrfldr in $fldr.Name){
+
+if(Get-ChildItem E:\"Dubai Vacation"\cloud\$othrfldr\ | Where-Object{$_.BaseName -match $AAE}){
+
+Write-Host "File found in" $othrfldr "Hence moving it there and deleting from AAE folder"
+Get-ChildItem -Path 'E:\Dubai Vacation\Apple_All_PhotosVideos\AAE' | Where-Object{$_.BaseName -match $AAE} | Move-Item -Destination E:\"Dubai Vacation"\cloud\\$othrfldr\ -Force 
+$AAE_file= $AAE_file| Where-Object{$_.basename -notmatch $AAE}
+break;
+}
+else{
+Write-Host "File Not found in" $othrfldr
+}
+
+}
+} #>
